fd6431df6279239232b0227c6dc5f12c8fc83398
Russell Fair <rfair404@gmail.com>
2016-08-02 07:43:04 -0400

    lazy

--- example.js
+++ example.js
@@ -1,6 +1,9 @@
 // This is the actual scratch extension part
 (function(ext) {
     var api_base = '';
+    var api_namespace = '';
+    var api_collection = '';
+    var api_url = '';
 
     // Cleanup function when the extension is unloaded
     ext._shutdown = function() {
@@ -24,12 +27,26 @@
     //callback for when base is set
     ext.set_api_base = function(api_base, value) {
         this.api_base = value;
-        console.log('setting api base to ' + value);
+        //console.log('setting api base to ' + value);
     };
-    ext.get_api_base = function() {
-        console.log('the api base is ' + this.api_base);
-        return this.api_base;
+    //callback for when namespace is set
+    ext.set_api_namespace = function(api_namespace, value) {
+        this.api_namespace = value;
+        //console.log('setting api namespace to ' + value);
     };
+    //callback for when collection is set
+    ext.set_api_collection = function(api_collection, value) {
+        this.api_collection = value;
+        //console.log('setting api collection to ' + value);
+    };
+
+    ext.get_api_url = function() {
+        this.api_url = [ this.api_base, this.api_namespace, this.api_collection];
+        this.api_url.join('/');
+        console.log('the api url route is ' +  this.api_url);
+        return this.api_url;
+    };
+
     ext._get = function(url) {
         //make a general get method later
     };
@@ -49,10 +66,13 @@
         blocks: [
             // ['h', 'Enable REST API', 'start'],
             [' ', 'Set %m.api_base to %s', 'set_api_base', 'api_base', 'https://example.com'],
+            [' ', 'Set %m.api_namespace to %s', 'set_api_namespace', 'api_namespace', 'wp/v2'],
+            [' ', 'Set %m.api_collection to %s', 'set_api_collection', 'api_collection', 'posts'],
+
             ['R', 'Discover API Routes', 'discover_routes'],
         ],
         menus : {
-          api_base: ['api_base', 'api_namespace', 'api_endpoint'],
+          api_base: ['api_base', 'api_namespace', 'api_collection'],
         },
         url: 'https://rfair404.github.io/scratch-rest'
         // Link to extension documentation, homepage, etc.
