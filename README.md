92f60c8160706f9dfa895a35de3f4ad86d56a949
Russell Fair <rfair404@gmail.com>
2016-08-02 06:19:02 -0400

    basic structure

---
+++ README.md
@@ -0,0 +1,13 @@
+=REST API Consumer for Scratch=
+This is an experimential scratchx plugin that allows you to consume REST API's using Scratch.
+
+===Project Goals===
+The goals for this project are:
+* Add basic support for REST API consuption that can be used as an experimential scratch extension.
+* Encourage people to use it, try it out and provide feedback.
+* Lobby for acceptance as a core scratch extension.
+
+===How to use this project===
+Visit http://scratchx.org/?url=http://rfair404.github.io/scratch-rest/example.js
+Use a "Discover Routes" block to generate a list of routes available from a REST endpoint.
+
--- example.js
+++ example.js
@@ -1,11 +1,32 @@
 // This is the actual scratch extension part
 (function(ext) {
-    // Register the extension
-    ScratchExtensions.register('scratch-rest', descriptor, ext);
+    var api_base = '';
+
+    //callback for when base is set
+    ext.set_api_base = function(api_base, value) {
+        this.api_base = value;
+    };
+    ext.get_api_base = function() {
+        return this.api_base;
+    };
+    ext._get = function(url) {
+        //make a general get method later
+    };
+    ext.discover_routes = function(callback) {
+    // Make an AJAX call to a given REST API and discover the available routes
+        $.ajax({
+            url: this.get_api_base(),
+            dataType: 'json',
+            success: function(ret){
+                console.log(ret);
+                callback(ret);
+            }
+        });
+    };
     var descriptor = {
         blocks: [
             // ['h', 'Enable REST API', 'start'],
-            [' ', 'Set %m.api_base to %s', 'set_api_base', 'api_base', 'http://rest-api-demo.q21.co/wp-json/'],
+            [' ', 'Set %m.api_base to %s', 'set_api_base', 'api_base', 'https://'],
             ['R', 'Discover API Routes', 'discover_routes'],
         ],
         menus : {
@@ -14,4 +35,8 @@
         url: 'http://rfair404.github.io/WP-REST-API-FOR-SCRATCHX'
         // Link to extension documentation, homepage, etc.
     };
+
+    // Register the extension
+    ScratchExtensions.register('scratch-rest', descriptor, ext);
+
 })({});
